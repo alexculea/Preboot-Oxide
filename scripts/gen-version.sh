abae3508205692a2f67a6092261f6642e1278e32
Alex Culea <16594531+alexculea@users.noreply.github.com>
2024-05-21 19:19:13 +0300

    chore: add version number gen script

---
+++ scripts/gen-version.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Initialize version numbers
+major=0
+minor=0
+patch=0
+
+# Fetch all commit messages
+commits=$(git log --pretty=format:%s)
+
+# Parse commit messages and update version numbers
+while IFS= read -r commit; do
+  if [[ $commit =~ ^feat ]]; then
+    ((minor++))
+  elif [[ $commit =~ ^fix ]]; then
+    ((patch++))
+  elif [[ $commit =~ ^refactor ]]; then
+    ((patch++))
+  elif [[ $commit =~ ^performance ]]; then
+    ((patch++))
+  fi
+
+  if [[ $commit =~ ^BREAKING\ CHANGE ]] || [[ $commit =~ '!:' ]]; then
+    ((major++))
+    minor=0
+    patch=0
+  fi
+done <<< "$commits"
+
+# Output the version number
+echo "$major.$minor.$patch"
+
+# [[ 'fix!: test'  =~ fix ]] && echo "yes"
