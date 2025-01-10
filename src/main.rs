9780f232ddd84f28b1803ceec2aea067bd39cedb
Alex Culea <16594531+alexculea@users.noreply.github.com>
2025-01-10 15:21:07 +0200

    chore: Simplifies some error returns and code formatting

--- src/main.rs
+++ src/main.rs
@@ -19,7 +19,7 @@ use preboot_oxide::{
 fn main() -> Result<()> {
     let instance = SingleInstance::new("preboot-oxide")?;
     if !instance.is_single() {
-        return Err(anyhow!("Another instance is already running"));
+        bail!("Another instance is already running");
     }
     let mut dot_env_path = env::current_exe().unwrap_or_default();
     dot_env_path.set_file_name(".env");
@@ -31,17 +31,18 @@ fn main() -> Result<()> {
         .or(env::var(format!("{ENV_VAR_PREFIX}LOG_LEVEL")).ok())
         .unwrap_or("error".into());
 
-    env_logger::Builder::from_env(env_logger::Env::default().default_filter_or(log_level))
-        .init();
+    env_logger::Builder::from_env(env_logger::Env::default().default_filter_or(log_level)).init();
 
     let conf_path = env::var(format!("{ENV_VAR_PREFIX}CONF_PATH"))
         .map(std::path::PathBuf::from)
         .ok();
-    let server_config = Conf::from_yaml_config(conf_path.as_ref())
-        .unwrap_or_else(|e| {
-            info!("Not loading YAML configuration: {}\nFalling back to environment variables.", e.to_string());
-            Conf::from(ProcessEnvConf::from_process_env())
-        });
+    let server_config = Conf::from_yaml_config(conf_path.as_ref()).unwrap_or_else(|e| {
+        info!(
+            "Not loading YAML configuration: {}\nFalling back to environment variables.",
+            e.to_string()
+        );
+        Conf::from(ProcessEnvConf::from_process_env())
+    });
     server_config.validate()?;
     spawn_tftp_service_async(&server_config)?;
 
